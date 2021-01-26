df07a368dd33cd24d30e2c06c997f843fd48e7f3
pinky0916 <varishtaja.kunjukuttan@gmail.com>
2021-01-25 22:25:00 -0800

    One line added

--- Simple interest.py
+++ Simple interest.py
@@ -3,9 +3,13 @@ T=int(input('Enter the time:'))
 R=float(input('Enter the rate:'))
 
 simple_interest=(P*T*R)/100
-print(simple_interest)
+print(f'simple_interest is :{simple_interest}')
 amount=(P*(1+R/100)**T)
-compound_interest=amount-P
-print(compound_interest)
+print(f'compound interest is :{amount-P}')
+
+#using pow()  instead of **
+amount1=P*(pow((1+R/100),T))
+print(f'Amount using pow function is :{amount1}')
+print(f'compound interest 2 is :{amount1-P}')
 
 print(1200*(1+5.4/100)**2)
