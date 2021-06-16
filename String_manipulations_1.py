1e6c82dc54568b44be1dce608d986cae613e085a
pinky0916 <varishtaja.kunjukuttan@gmail.com>
2021-06-16 13:29:36 -0700

    One line added

---
+++ String_manipulations_1.py
@@ -0,0 +1,24 @@
+#To include single quotes in a string-add it in double quotes
+print("Navin's job")
+
+#To print double quotes in a string
+print('Navin "JOB:"')
+
+
+#To print both single quotes and double quotes:
+print('Navin\'s "laptop"')
+
+
+#To concatenate two strings
+print('navin'+'navin')
+
+#To print 10 times
+print(10*'navin')
+
+print('************')
+print('c:\doc\navin')
+#\n is considered as new line so to print as user needs there are two ways:
+
+print('c:\doc\\navin')
+#using raw string
+print(r'c:\'\doc\navin\t\nagjhij')
