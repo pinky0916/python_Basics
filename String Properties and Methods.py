76704dd6c5f0c6840b49939236ee776c36855670
pinky0916 <varishtaja.kunjukuttan@gmail.com>
2021-06-16 18:25:58 -0700

    One line added

---
+++ String Properties and Methods.py
@@ -0,0 +1,27 @@
+#Concatenation of string:
+x="Hello Beautiful"
+x=x+" this is a sunny day"
+print(x)
+print ('*******************End of pgm1***********************')
+
+#print a string 10 times
+y="Star"
+y=y*10
+print(y)
+print ('*********************End of pgm2******************')
+
+#to print in upper case
+a='abcdefghij'
+print(a.upper())
+print ('*********************End of pgm3******************')
+
+#to print in upper case
+a='ABCDEF'
+print(a.lower())
+print ('*********************End of pgm3******************')
+
+#to split the string- returns a list
+a="Hi this is string"
+print(a.split())
+print(a.split('i'))
+print(a.split('i',2))
