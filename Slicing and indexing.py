c67fd2639a445207c103642e8e49e9847e25aedc
pinky0916 <varishtaja.kunjukuttan@gmail.com>
2021-06-16 18:38:12 -0700

    One line added

---
+++ Slicing and indexing.py
@@ -0,0 +1,17 @@
+#Indexing- to extract a single element/character in a string
+x='ABCDEF'
+print(x[2])
+print('*********End of pgm1*******')
+print(x[-1])
+print('*********End of pgm1*******')
+
+
+print('**********Slicing the string************')
+print(x[:3])
+print(x[2:4])
+print(x[::2])
+
+
+print('**********Reversing the string************')
+print(x[::-1])
+
