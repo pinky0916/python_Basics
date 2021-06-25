2ab12fa7fdba66038a9d47834b5084793eef41f6
pinky0916 <varishtaja.kunjukuttan@gmail.com>
2021-06-25 12:30:41 -0700

    One line added

---
+++ Basic_IF_template.py
@@ -0,0 +1,9 @@
+location='Travel'
+if location=='Auto':
+    print(f'the location is {location}')
+elif location=='Travel':
+    print('The location is {}'.format(location))
+elif location=='Bio diversity':
+    print(f'The location is {location}')
+else:
+    print(f"Location is {location}")
