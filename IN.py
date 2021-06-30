30155167a5f313e23090ba6552c71fa1754df361
pinky0916 <varishtaja.kunjukuttan@gmail.com>
2021-06-29 17:25:42 -0700

    One line added

---
+++ IN.py
@@ -0,0 +1,11 @@
+#To check if a value is avaialble in a list or string
+a='X' in ['X','Y','Z']
+print(a)
+print('********************')
+
+a='A' in ['a','b','c']
+print(a)
+print('********************')
+
+b= 'b' in ("I love Newyork")
+print(b)
